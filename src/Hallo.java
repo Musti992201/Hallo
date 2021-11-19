986fbfb04a7cb6c263573a0a823033125dada0c3
Mustafa Uyar <Mustafa.Uyar@outlook.at>
2021-11-19 18:43:46 +0100

    First test commit - neuaufsetzung

---
+++ src/Hallo.java
@@ -0,0 +1,11 @@
+public class Hallo {
+
+    public static void main(String[] args) {
+        int i=1;
+        int a=2;
+
+        System.out.println(i+a);
+
+        System.out.println("HALLO TEST!");
+    }
+}
